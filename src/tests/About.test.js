681765cffb933312a8c3ea2aa9ce827a680b14d4
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-20 17:58:21 -0300

    tests do about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+test('se o texto "About Pokédex" é renderizado na tela', () => {
+  const { getByText } = render(<About />);
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('se o 1° paragrafo de About é renderizado na tela', () => {
+  const { getByText } = render(<About />);
+  const ola = getByText(
+    'This application simulates a Pokédex, '
+    + 'a digital encliclopedia containing all Pokémons',
+  );
+  expect(ola).toBeInTheDocument();
+});
+
+test('se o 2° paragrafo de About é renderizado na tela', () => {
+  const { getByText } = render(<About />);
+  const ola = getByText(
+    'One can filter Pokémons by type, '
+    + 'and see more details for each one of them',
+  );
+  expect(ola).toBeInTheDocument();
+});
+
+test('se imagem da pokedex é renderizada', () => {
+  const { getByAltText } = render(<About />);
+  const ola = getByAltText('Pokédex');
+  expect(ola.src).toEqual('https://cdn.bulbagarden.net/upload/thumb/8/86/'
+  + 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -58,3 +58,8 @@ test('checks if links redirect to page ', () => {
   const erroMessage = getByText('Page requested not found');
   expect(erroMessage).toBeInTheDocument();
 });
+
+// test('checks if links redirect to page ', () => {
+//   const { getByText, history, getByTitle } = renderWithRouter(<App />);
+//   console.log(getByTitle('h1'));
+// });
