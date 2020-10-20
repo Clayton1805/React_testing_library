0350edef1637bcb0c4799e59df693df922b86e77
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-20 15:21:24 -0300

    testando se os links de navegaçao existem

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -13,12 +14,22 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-// test('shows the Pokédex when the route is `/`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter initialEntries={ ['/'] }>
-//       <App />
-//     </MemoryRouter>,
-//   );
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
 
-//   expect(getByText('Encountered pokémons')).toBeInTheDocument();
-// });
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('check if Home, About and Favorite Pokémon links exist', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText('Home');
+  expect(home).toBeInTheDocument();
+  const about = getByText('About');
+  expect(about).toBeInTheDocument();
+  const favoritePokemons = getByText('Favorite Pokémons');
+  expect(favoritePokemons).toBeInTheDocument();
+});
