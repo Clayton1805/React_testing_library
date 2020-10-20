07fd1e9804db8a3a020f5f7e182135456451f615
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-20 15:59:37 -0300

    testando se conferir os caminhos da urls esta funcionando

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
@@ -26,10 +26,35 @@ test('shows the Pokédex when the route is `/`', () => {
 
 test('check if Home, About and Favorite Pokémon links exist', () => {
   const { getByText } = renderWithRouter(<App />);
-  const home = getByText('Home');
-  expect(home).toBeInTheDocument();
-  const about = getByText('About');
-  expect(about).toBeInTheDocument();
-  const favoritePokemons = getByText('Favorite Pokémons');
-  expect(favoritePokemons).toBeInTheDocument();
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+});
+
+test('checks if link "About" redirect to URL /about', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText('About'));
+  const { pathname } = history.location;
+  expect(pathname).toEqual('/about');
+});
+
+test('checks if link "Favorite Pokémons" redirect to URL /favorites', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText('Favorite Pokémons'));
+  const { pathname } = history.location;
+  expect(pathname).toEqual('/favorites');
+});
+
+test('checks if link "Home" redirect to URL /', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText('Home'));
+  const { pathname } = history.location;
+  expect(pathname).toEqual('/');
+});
+
+test('checks if links redirect to page ', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/hoje-não');
+  const erroMessage = getByText('Page requested not found');
+  expect(erroMessage).toBeInTheDocument();
 });
