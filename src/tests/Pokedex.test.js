a54aaf055578e624ec9c3572d891679961d29542
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-21 16:57:30 -0300

    test lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+test('', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const namesAllPokemons = pokemons.map((pokemon) => pokemon.name);
+  namesAllPokemons.push(...namesAllPokemons.slice(0, 5));
+  namesAllPokemons.forEach((name, index) => {
+    if (index === 0) {
+      expect(getByText(name)).toBeInTheDocument();
+    } else {
+      fireEvent.click(getByText('Próximo pokémon'));
+      expect(getByText(name)).toBeInTheDocument();
+    }
+  });
+});
