9874415760d0fd5e3fc98fbdfe37fe4697a41bfe
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-24 18:38:15 -0300

    tests do pokedex no fim

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -7,14 +7,52 @@ import pokemons from '../data';
 test('', () => {
   const { getByText } = renderWithRouter(<App />);
 
-  const namesAllPokemons = pokemons.map((pokemon) => pokemon.name);
-  namesAllPokemons.push(...namesAllPokemons.slice(0, 5));
-  namesAllPokemons.forEach((name, index) => {
-    if (index === 0) {
+  const allPokemons = [...pokemons, ...pokemons];
+
+  allPokemons.forEach(({ name }, index) => {
+    const firstIndex = 0;
+    if (index === firstIndex) {
       expect(getByText(name)).toBeInTheDocument();
     } else {
       fireEvent.click(getByText('Próximo pokémon'));
       expect(getByText(name)).toBeInTheDocument();
     }
   });
+});
+
+test('', () => {
+  const { getAllByTestId } = renderWithRouter(<App />);
+
+  expect(getAllByTestId('pokemon-name').length).toEqual(1);
+});
+
+test('', () => {
+  const { getByText, getAllByTestId } = renderWithRouter(<App />);
+
+  const allPokemonsTypes = pokemons.map((pokemon) => pokemon.type);
+  const allTypes = allPokemonsTypes
+    .filter((type, i) => allPokemonsTypes.indexOf(type) === i);
+
+  allTypes.forEach((type) => {
+    const buttonType = getAllByTestId('pokemon-type-button')
+      .find((button) => button.innerHTML === type);
+    expect(buttonType).toBeInTheDocument();
+    fireEvent.click(buttonType);
+
+    const pokemonsHaveType = pokemons.filter((pokemon) => pokemon.type === type);
+    pokemonsHaveType.push(...pokemonsHaveType);
+    pokemonsHaveType.forEach(({ name }, index) => {
+      const firstIndex = 0;
+      if (index === firstIndex) {
+        expect(getByText(name)).toBeInTheDocument();
+      } else {
+        fireEvent.click(getByText('Próximo pokémon'));
+        expect(getByText(name)).toBeInTheDocument();
+      }
+    });
+  });
+});
+
+test('', () => {
+
 });
