71d5956fdfb72ca776ff71e955f4e0a421f601d8
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-10-25 09:12:58 -0300

    começando PokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+// import pokemons from '../data';
+
+test('1', () => {
+  const { getByText, queryByText } = renderWithRouter(<App />);
+  // fazer um mock da requisição de mais detalhes cujo link é dado no obj
+
+  fireEvent.click(getByText('More details'));
+
+  expect(getByText('Pikachu Details')).toBeInTheDocument();
+  expect(queryByText('More details')).toBeNull();
+  expect(getByText('Summary')).toBeInTheDocument();
+  // expect(getByText()).toBeInTheDocument();
+
+  fireEvent.click(getByText('Home'));
+});
